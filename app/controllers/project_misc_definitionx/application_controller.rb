e1ef1d2cf8066723cf73e1c1ab71bc9a99c020eb
jc <emclab2011@gmail.com>
2014-07-08 15:56:39 -0500

    JC: removed set_local in app controller.

--- app/controllers/project_misc_definitionx/application_controller.rb
+++ app/controllers/project_misc_definitionx/application_controller.rb
@@ -7,7 +7,6 @@ module ProjectMiscDefinitionx
     include Commonx::CommonxHelper
     
     before_filter :require_signin
-    before_filter :set_locale
     before_filter :max_pagination 
     before_filter :check_access_right 
     before_filter :load_session_variable, :only => [:new, :edit]  #for parent_record_id & parent_resource in check_access_right
