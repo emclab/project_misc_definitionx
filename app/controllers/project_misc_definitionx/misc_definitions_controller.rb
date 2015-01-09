26343c126032d355b2453f29b8ba26c68fd595f1
jc <emclab2011@gmail.com>
2015-01-09 17:10:03 -0600

    JC: removed @definition_category from the view name. spec passes.

--- app/controllers/project_misc_definitionx/misc_definitions_controller.rb
+++ app/controllers/project_misc_definitionx/misc_definitions_controller.rb
@@ -11,13 +11,14 @@ module ProjectMiscDefinitionx
       @misc_definitions = @misc_definitions.where(definition_category: @definition_category) if @definition_category
       @misc_definitions = @misc_definitions.where(project_id: @project.id) if @project
       @misc_definitions = @misc_definitions.page(params[:page]).per_page(@max_pagination)
-      @erb_code = find_config_const('misc_definition_index_view_' + @definition_category, 'project_misc_definitionx')
+      @erb_code = find_config_const('misc_definition_index_view', 'project_misc_definitionx')
+      #@erb_code = find_config_const('misc_definition_index_view_' + @definition_category, 'project_misc_definitionx')
     end
   
     def new
       @title = t('New Misc Definition')
       @misc_definition = ProjectMiscDefinitionx::MiscDefinition.new()
-      @erb_code = find_config_const('misc_definition_new_view_' + @definition_category, 'project_misc_definitionx')
+      @erb_code = find_config_const('misc_definition_new_view', 'project_misc_definitionx')
     end
   
     def create
@@ -29,7 +30,7 @@ module ProjectMiscDefinitionx
                                                                :notice => t("Successfully Saved!") 
       else
         @project = ProjectMiscDefinitionx.project_class.find_by_id(params[:misc_definition][:project_id]) if params[:misc_definition][:project_id].present?
-        @erb_code = find_config_const('misc_definition_new_view_' + @definition_category, 'project_misc_definitionx')
+        @erb_code = find_config_const('misc_definition_new_view', 'project_misc_definitionx')
         flash[:notice] = t('Data Error. Not Saved!')
         render 'new'
       end
@@ -38,7 +39,7 @@ module ProjectMiscDefinitionx
     def edit
       @title = t('Update Misc Definition')
       @misc_definition = ProjectMiscDefinitionx::MiscDefinition.find_by_id(params[:id])
-      @erb_code = find_config_const('misc_definition_edit_view_' + @definition_category, 'project_misc_definitionx')
+      @erb_code = find_config_const('misc_definition_edit_view', 'project_misc_definitionx')
     end
   
     def update
@@ -49,7 +50,7 @@ module ProjectMiscDefinitionx
         redirect_to misc_definitions_path(:project_id => @misc_definition.project_id, :definition_category => @misc_definition.definition_category, :subaction => session[:subaction] ), 
                                                                :notice => t("Successfully Updated!") 
       else
-        @erb_code = find_config_const('misc_definition_edit_view_' + @definition_category, 'project_misc_definitionx')
+        @erb_code = find_config_const('misc_definition_edit_view', 'project_misc_definitionx')
         flash[:notice] = t('Data Error. Not Updated!')
         render 'edit'
       end
@@ -58,7 +59,7 @@ module ProjectMiscDefinitionx
     def show
       @title = t('Misc Definition Info')
       @misc_definition = ProjectMiscDefinitionx::MiscDefinition.find_by_id(params[:id])
-      @erb_code = find_config_const('misc_definition_show_view_' + @definition_category, 'project_misc_definitionx')
+      @erb_code = find_config_const('misc_definition_show_view', 'project_misc_definitionx')
     end
   
     def destroy
--- lib/project_misc_definitionx/version.rb
+++ lib/project_misc_definitionx/version.rb
@@ -1,3 +1,3 @@
 module ProjectMiscDefinitionx
-  VERSION = "3.2.12.05"
+  VERSION = "3.2.12.06"
 end
